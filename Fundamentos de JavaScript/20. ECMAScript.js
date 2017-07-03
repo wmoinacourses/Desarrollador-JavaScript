cc37f2fac499e2aede41b2e4fa8ce757a96a0213
Wilmer Moina Courses <wilmermoinacourses@gmail.com>
2017-07-03 17:42:00 -0500

    Fundamentos - 5. ECMAScript 5, 2015, 2016, Babel y otros cuentos

---
+++ Fundamentos de JavaScript/20. ECMAScript.js
@@ -0,0 +1,10 @@
+CMAScript: El estándar en el que se basa JavaScript
+El estándar en el que se basa JavaScript.
+Comenzó llamándose LiveScript para lo que se mostraba al cliente.
+Java != JavaScript
+Se llamó ECMAScript porque fue desarrollado por ECMA, una empresa dedicada a telecomunicaciones.
+ECMA-262
+Sus primeras versiones fueron lanzadas en los 90’s.
+En este momento se está creando ECMAScript 2018.
+Si escribimos código con ECMAScript tiene que ser compatible con el navegador en el que queremos que corra.
+En el siguiente vamos a hablar sobre BABEL.
